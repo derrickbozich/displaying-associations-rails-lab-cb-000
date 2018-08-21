32dabae63b45cb445090cb4a5e16badbec2faec8
derrick b <dbozich1@gmail.com>
2018-08-21 18:41:02 +0000

    automatically backed up by learn

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,9 +2,11 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-      
+
 
       t.timestamps null: false
     end
   end
 end
+
+# rails generate migration add_artist_id_to_songs artist_id:integer
